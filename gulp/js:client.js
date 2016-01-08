3570c8de37582e2f16239c5bc88a93693fc0d6af
Andreas Zwettler <azwettler.mmt-b2013@fh-salzburg.ac.at>
2016-01-08 11:36:09 +0100

    [CHORE] ignore socket.io js while hinting

--- gulp/js:client.js
+++ gulp/js:client.js
@@ -1,11 +1,13 @@
 var gulp = require('gulp');
 var jshint = require('gulp-jshint');
 var uglify = require('gulp-uglifyjs');
+var addsrc = require('gulp-add-src');
 
 module.exports = [['clean:js:client'], function() {
-	return gulp.src(['app/clientJs/**/*.js','node_modules/socket.io/node_modules/socket.io-client/socket.io.js'])
+	return gulp.src('app/clientJs/**/*.js')
 	.pipe(jshint())
 	.pipe(jshint.reporter('jshint-stylish'))
+	.pipe(addsrc('node_modules/socket.io/node_modules/socket.io-client/socket.io.js'))
 	.pipe(uglify('main.min.js', {
 		mangle: false,
 		output: {
